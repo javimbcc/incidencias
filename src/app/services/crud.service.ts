5934cc302ca750f21bb7a64cd38d84fbeab4b076
JAVIER\menaj <csi22-jmennza@colegioaltair.net>
2023-02-06 13:05:59 +0100

    inicio

---
+++ src/app/services/crud.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CrudService {
+
+  constructor(private firestore: AngularFirestore) { }
+
+  cogerTodos(coleccion: string) {
+    return this.firestore.collection(coleccion).snapshotChanges();
+  }
+  //Metodo para coger un documento de la base de datos
+  cogerUno(coleccion: string, documentId: string) {
+    return this.firestore.collection(coleccion).doc(documentId).snapshotChanges();
+  }
+  //Metodo para actualizar un documento de la base de datos
+  Actualizar(coleccion: string, documentId: string, data: any) {
+    return this.firestore.collection(coleccion).doc(documentId).update(data);
+  }
+  //Metodo para crear un documento de la base de datos
+  Crear(coleccion: string, data: any) {
+    return this.firestore.collection(coleccion).add(data);
+  }
+  //Metodo para eliminar un documento de la base de datos
+  Eliminar(coleccion: string, documentId: string) {
+    return this.firestore.collection(coleccion).doc(documentId).delete();
+  }
+
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'incidencias-3c103',
+    appId: '1:622002017004:web:c7e638ce4e276e9bf1cbc9',
+    storageBucket: 'incidencias-3c103.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBpnGr_0nik0eMj338OMhvCW0ify0y2TH4',
+    authDomain: 'incidencias-3c103.firebaseapp.com',
+    messagingSenderId: '622002017004',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
